9d4d0cf16bbbd49add60f95b54d0c7c12635650c
wongivy@uw.edu <wongivy@uw.edu>
2016-01-26 11:43:15 -0800

    End of First hour

--- app/src/main/java/edu/uw/intentdemo/MainActivity.java
+++ app/src/main/java/edu/uw/intentdemo/MainActivity.java
@@ -2,6 +2,7 @@ package edu.uw.intentdemo;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "**DEMO**";
 
+    private static final int REQUEST_PICTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
                 Log.v(TAG, "Launch button pressed");
 
 
+                //Explicit Intent
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("edu.uw.intentdemo.message", "Hello number 2!");
+
+                startActivity(intent);
+
+
             }
         });
 
@@ -36,6 +45,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.v(TAG, "Call button pressed");
 
+                //implicit intent
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:206-685-1622"));
+
+                if (intent.resolveActivity(getPackageManager()) != null)
+                    startActivity(intent);
 
             }
         });
@@ -45,6 +60,10 @@ public class MainActivity extends AppCompatActivity {
         cameraButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.v(TAG, "Camera button pressed");
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null)
+                    startActivityForResult(intent, REQUEST_PICTURE);
 
 
             }
@@ -59,8 +78,20 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        //if camera responds with a "yup!"
+        if (requestCode == REQUEST_PICTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap)extras.get("data");
 
+            ((ImageView)findViewById(R.id.imgThumbnail)).setImageBitmap(bitmap);
+        }
+
+    }
 }
--- app/src/main/java/edu/uw/intentdemo/SecondActivity.java
+++ app/src/main/java/edu/uw/intentdemo/SecondActivity.java
@@ -1,10 +1,13 @@
 package edu.uw.intentdemo;
 
+import android.content.Intent;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.AdapterView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -22,6 +25,13 @@ public class SecondActivity extends AppCompatActivity {
         //action bar "back"
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String msg = extras.getString("edu.uw.intentdemo.message");
+
+        Log.v(TAG, msg);
+
+        ((TextView) findViewById(R.id.txtSecond)).setText(msg);
 
         //TODO: fill in the link between columns and ids!
         String[] cols = new String[]{};
